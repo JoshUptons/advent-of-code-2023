88c94b00157c5f2b60123075778abd2aade619be
JoshUptons <itsjoshupton@gmail.com>
2023-12-02 20:30:44 -0600

    added day1 problem 1 solution

---
+++ day1/problem_one/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func handleErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var total int
+	file, err := os.Open("./input.txt")
+	handleErr(err)
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// parse line into bytes
+	for scanner.Scan() {
+		var digits []int
+		lineBytes := scanner.Bytes()
+		// parse bytes into characters
+		for _, b := range lineBytes {
+			if digit, err := strconv.Atoi(string(b)); err == nil {
+				// is a digit
+				digits = append(digits, digit)
+			}
+		}
+		// combine the numbers to add them to the total
+		num, err := strconv.Atoi(
+			fmt.Sprintf(
+				"%d%d",
+				digits[0],
+				digits[len(digits)-1],
+			),
+		)
+		handleErr(err)
+		// finally add them to the total
+		total += num
+	}
+
+	if scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	fmt.Println(total)
+
+}
